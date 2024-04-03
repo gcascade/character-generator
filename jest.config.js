591af5e87f92408fd8230c475f2d5dc13d864f8a
gcascade <geoffroy.cascade@reseau.eseo.fr>
2024-04-03 22:29:11 +0200

    Update test.yml and cssTransform.js, remove jest.config.js, and update package.json

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-};
