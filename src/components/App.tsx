ac0f6eff0447721969f1e133cf8f8feea7b85efd
gcascade <geoffroy.cascade@reseau.eseo.fr>
2024-05-21 21:49:55 +0200

    Update background color in App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -25,7 +25,7 @@ const App: React.FC = () => {
             <BrowserRouter>
               <Box
                 style={{
-                  backgroundColor: 'red',
+                  backgroundColor: theme.palette.background.paper,
                 }}
               >
                 <CssBaseline />
