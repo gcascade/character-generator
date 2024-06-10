289c0de0897921cb3c23cd0ecb91d6481bf58b14
gcascade <geoffroy.cascade@reseau.eseo.fr>
2024-06-10 21:01:51 +0200

    chore: Update AlertManagerContext import formatting in test file

--- src/contexts/AlertManagerContext.test.tsx
+++ src/contexts/AlertManagerContext.test.tsx
@@ -1,8 +1,8 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import {
-    AlertManagerContext,
-    AlertManagerProvider,
+  AlertManagerContext,
+  AlertManagerProvider,
 } from './AlertManagerContext';
 
 describe('AlertManagerContext', () => {
