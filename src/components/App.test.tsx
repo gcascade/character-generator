2c5abde2f67e7018138d830cb1eb64e29424f8a2
gcascade <geoffroy.cascade@reseau.eseo.fr>
2024-07-03 23:31:56 +0200

    Refactor import statement for Home component in App.test.tsx

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import App from './App';
 
-jest.mock('../pages/Home/Home', () => () => <div>CharacterGenerator</div>);
+jest.mock('../pages/home/Home', () => () => <div>CharacterGenerator</div>);
 
 describe('App', () => {
   test('renders App component without crashing', () => {
